44ec3dd4c59d3117dbe5cc1197f94b9d451a9faf
武哥 <63054122+itwuge@users.noreply.github.com>
2022-12-03 15:10:00 +0800

    Update diy-x86-Lienol.sh

--- diy-x86-Lienol.sh
+++ diy-x86-Lienol.sh
@@ -10,13 +10,13 @@
 #sed -i '5s/Lienol/sypopo/g' ./feeds.conf.default
 
 echo '修改网关地址'
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.2/g' package/base-files/files/bin/config_generate
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-#echo '修改机器名称'
-#sed -i 's/OpenWrt/OpenWrt-x64/g' package/base-files/files/bin/config_generate
+echo '修改机器名称'
+sed -i 's/OpenWrt/OpenWrt-wuge-x64/g' package/base-files/files/bin/config_generate
 
 echo '修改banner'
 cp -f diy/sypopo/banner package/base-files/files/etc/
